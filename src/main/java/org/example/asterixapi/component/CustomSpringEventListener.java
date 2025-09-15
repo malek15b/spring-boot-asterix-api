2bd0ca036b1cd74ecdce117855445e5225606804
malek.b. <malek.boulakbech@gmail.com>
2025-09-15 15:44:56 +0200

    add event request

--- src/main/java/org/example/asterixapi/component/CustomSpringEventListener.java
+++ src/main/java/org/example/asterixapi/component/CustomSpringEventListener.java
@@ -3,6 +3,7 @@ package org.example.asterixapi.component;
 import org.example.asterixapi.event.CustomSpringEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.ServletRequestHandledEvent;
 
 @Component
 public class CustomSpringEventListener {
@@ -10,5 +11,12 @@ public class CustomSpringEventListener {
     @EventListener
     public void handleUserCreated(CustomSpringEvent event) {
         System.out.println("Event recipe: Result -> " + event.getMessage());
+    }
+
+    @EventListener
+    public void handleRequest(ServletRequestHandledEvent event) {
+        System.out.println("Request handled: " + event.getRequestUrl()
+                + " Status=" + event.getStatusCode());
+        System.out.println(event.getClientAddress());
     }
 }
